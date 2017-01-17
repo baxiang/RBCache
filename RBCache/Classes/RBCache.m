cb9e15b1f2af176e02972d3a4280823c37b58737
baxiang <baxiang1989@163.com>
2017-01-18 01:06:47 +0800

    框架

--- RBCache/Classes/RBCache.m
+++ RBCache/Classes/RBCache.m
@@ -10,6 +10,13 @@
 
 @implementation RBCache
 
-
++(RBCache*)defaultCache{
+    static RBCache *_dafaultCache;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _dafaultCache = [[[self class] alloc] init];
+    });
+    return _dafaultCache;
+}
 
 @end
